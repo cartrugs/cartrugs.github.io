e82b9aa402568f5cc27894fd32d4ef6e81e8f8eb
cartrugs <cartrugs@gmail.com>
2023-07-13 22:36:12 +0200

    Corrección de errores en input búsqueda y corrección de fallos llamada API. PINTAR búsqueda conseguido. corrección para js de html para búsqueda

--- js/main.js
+++ js/main.js
@@ -1,34 +1,39 @@
 /**
  * Se ejecuta cuando el contenido del DOM ha sido cargado.
  */
-document.addEventListener('DOMContentLoaded', () => {
-
+document.addEventListener("DOMContentLoaded", () => {
   // VARIABLES
-  // querySelector botón e input
 
   /**
    *  Botón que al clicar inicia la búsqueda (tipo objeto del DOM).
    * @type {object}
    */
-  const botonBuscar = document.querySelector('#botonBuscar');
+  const botonBuscar = document.querySelector("#botonBuscar");
 
   /**
    *  Elemento input de entrada de búsqueda (tipo objeto del DOM). Se agrega al final .value Ejemplo(entre^^): ^querySelector().value^ para capturar el valor del input (palabra con que el usuario genera la búsqueda) y se asigna ese valor como argumemnto a la función encargada de arrojar los resultados.
    * @type {object}
    */
-  const inputBuscar = document.querySelector('#inputBuscar').value;
-  
-   /**
-   * El div donde van a mostrase los resultados de la búsqueda..
+
+
+  /**
+   * El formulario para buscar resultados.
    * @type {object}
    */
-  const resultadoBusqueda = document.querySelector('#resultadoBusqueda');
+  const formularioBuscar = document.querySelector("#idFormulario");
 
-   /**
+
+  /**
+   * El div donde van a mostrase los resultados de la búsqueda.
+   * @type {object}
+   */
+  const resultadoBusqueda = document.querySelector("#resultadoBusqueda");
+
+  /**
    * El div donde van a colocarse las tres fotos tendencias.
    * @type {object}
    */
-  const photosPrincipales = document.querySelector('#photosPrincipales');
+  const photosPrincipales = document.querySelector("#photosPrincipales");
 
   /**
    * Expresión regular que permite cualquier palabra o palabras con una longitud máxima de 50 caracteres.
@@ -46,7 +51,7 @@ document.addEventListener('DOMContentLoaded', () => {
    * La URL base para la API de Pexels.
    * @type {string}
    */
-  const urlBase = 'https://api.pexels.com/v1';
+  const urlBase = "https://api.pexels.com/v1";
 
   // EVENTOS
 
@@ -54,21 +59,23 @@ document.addEventListener('DOMContentLoaded', () => {
    *  * @param {Event}
    * Evento click en el botón de búsqueda. Se pone a la escucha al documento y se asigna target al botón para desencadenar el evento.
    */
-  document.addEventListener('click', ({ target }) => {
+  formularioBuscar.addEventListener("submit", (ev) => {
+    ev.preventDefault()
 
-    if (target === document.getElementById('#botonBuscar')) {
-        // Variable para crear búsqueda a través del valor del input, introduciendo palabra en el elemento.
-        // const datosBusqueda = inputBuscar.value;
-        // Método test para comprobar la expresión regular(true si hay coincidencia)
-        if (regExp.test(datosBusqueda)) {
-            // Si true, se ejecuta función ejecutarBusqueda
-            pintarResultados();
-          } else {
-            console.log('valor de la búsqueda no válido');
-          }
-        } 
-        
-    
+    console.log(ev);
+    if (ev.target.className == 'btn') {
+
+      const palabraUsuario = document.getElementById('palabra').value
+  
+      
+      // Método test para comprobar la expresión regular(true si hay coincidencia)
+      if (regExp.test(palabraUsuario)) {
+        // Si true, se ejecuta función ejecutarBusqueda
+        pintarResultados(palabraUsuario);
+      } else {
+        console.log("valor de la búsqueda no válido");
+      }
+    }
   });
 
   // FUNCIONES
@@ -76,65 +83,63 @@ document.addEventListener('DOMContentLoaded', () => {
   // Función para llevar a cabo la búsqueda
   const ejecutarBusqueda = async (url) => {
     try {
-
       // const urlBuscar = `search?query=${inputBuscar.value}`
       // Retorna promesa. await para que continúe ejecutándose el programa. fetch para obtener recursos de forma asíncrona por la red
       let res = await fetch(`${urlBase}/${url}`, {
-        
         // No tengo muy claro si esto es así
         headers: {
-          'Authorization': 'aIwCSd1ODcT15TJOIvWuLrgCFSgq5Krev7gA8CV5IhQFpAPqt7eA65LU',
+          Authorization:
+            "aIwCSd1ODcT15TJOIvWuLrgCFSgq5Krev7gA8CV5IhQFpAPqt7eA65LU",
         },
       });
 
       if (res.ok) {
         res = await res.json();
-        
+
         return res;
       } else {
-        throw 'Ha ocurrido un error';
+        throw "Ha ocurrido un error";
       }
     } catch (error) {
       console.log(error);
     }
   };
 
+  const urlDinamica = "https://api.pexels.com/v1/search?query=red&per_page=1"
+
   // Función para mostrar los resultados de búsqueda
-  const pintarResultados = async () => {
+  const pintarResultados = async (url) => {
     // Array para almacenar las imágenes que obtenga de pexels
-    const busquedaArray = await ejecutarBusqueda(`search?query=${inputBuscar.value}&page=1`);
-    console.log(busquedaArray)
-    console.log(busquedaArray.photos.length);
-    busquedaArray.photos.forEach(({ alt, src }) => {
-      const cajasPhotos = document.createElement('DIV');
-      cajasPhotos.classList.add('caja-photo');
-      const imageBusqueda = document.createElement('IMG');
-      imageBusqueda.setAttribute('src', src); // Corrección: establecer el atributo src correctamente
-      console.log(src)
-      imageBusqueda.setAttribute('alt', alt); // Corrección: establecer el atributo alt correctamente
-      const figCaption = document.createElement('FIGCAPTION');
+    
+    const busquedaArray = await ejecutarBusqueda(`search?query=${url}`);
+    console.log(busquedaArray);
+    busquedaArray.photos.forEach((item) => {
+      // console.log(item);
+      const cajasPhotos = document.createElement("FIGURE");
+      cajasPhotos.classList.add("caja-photo");
+      const imageBusqueda = document.createElement("IMG");
+      (imageBusqueda.src = item.src.medium),
+        imageBusqueda.setAttribute("alt", item.alt);
+      const figCaption = document.createElement("FIGCAPTION");
       figCaption.append(imageBusqueda);
       cajasPhotos.append(figCaption);
       resultadoBusqueda.append(cajasPhotos);
     });
-    
   };
 
-  // const crearEstaticas = async () => { 
+  // const crearEstaticas = async () => {
   //   const {ok, page, photos} = await ejecutarBusqueda()
   //   const cajasPhotosTendencias = document.createElement('DIV');
   //     cajasPhotosTendencias.classList.add('caja-photo-tendencias');
   //     const imageTendencias = document.createElement('IMG');
-      
 
   //     imageTendencias.setAttribute('id', item);
   //     const figCaption = document.createElement('FIGCAPTION');
   //     figCaption.append(imageTendencias);
   //     imageTendencias.append(cajasPhotos);
 
-  // } 
+  // }
 
   // INVOCACIÓN FUNCIONES
-  pintarResultados()
 
 }); // LOAD
